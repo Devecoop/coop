3d828496a275e09cf94087b8276c7b15c57edb08
Juan Manuel Schillaci <juan.schillaci@devecoop.com>
2013-11-23 15:40:54 -0300

    Added missing GPLv3 headers

--- lib/constants.py
+++ lib/constants.py
@@ -1,3 +1,23 @@
+#  Coop a tool to deploy and socialize Python projects
+#   Copyright (C) 2013  Juan Manuel Schillaci
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+# Coop version 0.1, Copyright (C) 2013  Juan Manuel Schillaci
+#   Coop comes with ABSOLUTELY NO WARRANTY.
+#   This is free software, and you are welcome to redistribute it
+#   under certain conditions;
 import os
 
 COOP_MAIN_PATH = '.coop'
