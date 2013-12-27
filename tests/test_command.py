4f1ce955ca6875668679dce832257719d28d2e8e
Juan Manuel Schillaci <juan.schillaci@devecoop.com>
2013-12-26 22:37:51 -0300

    Added test to check the case of wrong extension names

--- tests/test_command.py
+++ tests/test_command.py
@@ -31,6 +31,7 @@ DUMMY_COMMAND_1 = 'dummy1'
 DUMMY_COMMAND_2 = 'dummy2'
 DUMMY_COMMAND_3 = 'dummy3'
 COMMAND_EXTENSION_SUFFIX = ".py"
+COMMAND_EXTENSION_WRONG_SUFFIX = ".pyc"
 
 DUMMY_COMMAND_1_PATH = os.path.join(COOP_TEST_COMMAND_PATH,
                                     DUMMY_COMMAND_1 + COMMAND_EXTENSION_SUFFIX)
@@ -40,6 +41,9 @@ DUMMY_COMMAND_2_PATH = os.path.join(COOP_TEST_COMMAND_PATH,
 
 DUMMY_COMMAND_3_PATH = os.path.join(COOP_TEST_COMMAND_PATH,
                                     DUMMY_COMMAND_3 + COMMAND_EXTENSION_SUFFIX)
+
+DUMMY_COMMAND_WRONG_4_PATH = os.path.join(COOP_TEST_COMMAND_PATH,
+                                    DUMMY_COMMAND_3 + COMMAND_EXTENSION_WRONG_SUFFIX)
 
 
 
@@ -51,7 +55,7 @@ class TestCommandDiscover(unittest.TestCase):
         except OSError:
             pass
 
-    def test_given_no_files_are_present_then_return_empty_list(self):
+    def test_given_no_files_are_present_when_calling_discover_commands_return_empty_list(self):
         try:
             if not os.path.exists(COOP_TEST_COMMAND_PATH):
                 os.mkdir(COOP_TEST_COMMAND_PATH)
@@ -61,7 +65,7 @@ class TestCommandDiscover(unittest.TestCase):
         finally:
             os.rmdir(COOP_TEST_COMMAND_PATH)
 
-    def test_given_files_are_present_then_return_list_with_file_names(self):
+    def test_given_files_are_present_when_calling_discover_commands_return_list_with_file_names(self):
         try:
             if not os.path.exists(COOP_TEST_COMMAND_PATH):
                 os.mkdir(COOP_TEST_COMMAND_PATH)
@@ -76,10 +80,25 @@ class TestCommandDiscover(unittest.TestCase):
                                DUMMY_COMMAND_3]
             result = discover_commands(CURRENT_PATH)
             self.assertEqual(expected_result.sort(), result.sort())
-
         finally:
             shutil.rmtree(COOP_TEST_COMMAND_PATH)
 
+    def test_given_files_with_mixed_extensions_are_present_when_calling_discover_commands_return_only_valid_file_names(self):
+        try:
+            if not os.path.exists(COOP_TEST_COMMAND_PATH):
+                os.mkdir(COOP_TEST_COMMAND_PATH)
+            fp1 = open(DUMMY_COMMAND_1_PATH, "wb")
+            fp1.close()
+            fp1 = open(DUMMY_COMMAND_2_PATH, "wb")
+            fp1.close()
+            fp1 = open(DUMMY_COMMAND_WRONG_4_PATH, "wb")
+            fp1.close()
+
+            expected_result = [DUMMY_COMMAND_1, DUMMY_COMMAND_2]
+            result = discover_commands(CURRENT_PATH)
+            self.assertEqual(expected_result.sort(), result.sort())
+        finally:
+            shutil.rmtree(COOP_TEST_COMMAND_PATH)
 
 
 if __name__ == '__main__':
